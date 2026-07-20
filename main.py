d1077d732c19d88770801a65e0be46f7cdd109ec
Pavel <talgos61@gmail.com>
2026-07-20 21:58:56 +0300

    Fixing bugs

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def schedule_func(bot): # Не забудь передать bot!
 
   
     from consumables import auto_consumables_report
-    schedule.every().monday.at("9:10:00", 'Europe/Moscow').do(auto_consumables_report, bot)
+    schedule.every().monday.at("09:10:00", 'Europe/Moscow').do(auto_consumables_report, bot)
     # --- СТАТИЧЕСКИЕ ЗАДАЧИ КЛУБОВ ---
     # Например, принудительное закрытие в 05:00 (если оно всегда в 5 утра)
     # Можно оставить так, пробежавшись один раз при старте
